f5e9d891cd0b4cd6143e0bf5d40b80e53ff7fac4
Jonathan Wheeler <jonathan.wheeler01@gmail.com>
2022-02-03 14:37:31 -0700

    revised workflow done

--- data/combined_view.sql
+++ data/combined_view.sql
@@ -1,6 +1,6 @@
 -- Create a combined view of the three tables
 -- CREATE VIEW combined AS
-SELECT surveys.*, plots.plot_type, species.genus, species.species, species.taxa
+SELECT surveys.*, species.genus, species.species, species.taxa, plots.plot_type
 FROM surveys
-INNER JOIN plots ON surveys.plot_id = plots.plot_id
 INNER JOIN species ON surveys.species_id = species.species_id
+INNER JOIN plots ON surveys.plot_id = plots.plot_id
